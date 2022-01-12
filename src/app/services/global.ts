bcaf20baa0aaa46891509bd1f456ddcf30bc502a
JoseMR <jose8188258@gmail.com>
2022-01-11 20:04:51 -0400

    #DespliegueNubeDONE

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export var global={
-    // url: 'https://modeloc4.herokuapp.com/api/'
-    url:'http://localhost:3000/api/'
+    url: 'http://yo-estudiante.herokuapp.com/api/'
+    // url:'http://localhost:3000/api/'
      
   };
