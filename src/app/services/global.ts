99356cc08d6f7a114c1975cee2ba422eef062ed8
elena750 <eguezrodas@gmail.com>
2022-07-14 14:43:59 -0400

    test

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export var global={
-    url: 'https://yo-estudiante.herokuapp.com/api/'
-    // url:'http://localhost:3000/api/'
+   // url: 'https://yo-estudiante.herokuapp.com/api/'
+     url:'http://localhost:3000/api/'
      
   };
